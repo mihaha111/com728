c051a8a61ebfb6e0db05a3d75b95b15dfd0a3d3a
Mihaha111 <5handm53@solent.ac.uk>
2021-02-16 21:14:55 -0800

    ascii_art

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,8 @@
+# Added Code to Display a box
+print("\n\n\t\t\t\t BBBBBBB\t\t\t\t\t")
+print("\t\t\t\t B _ _ B \t\t\t\t")
+print("\t\t\t\tB  O O  B\t\t\t\t")
+print("\t\t\t  BBB       BBB\t\t\t\t")
+print("\t\t\t   BB  <P>  BB\t\t\t\t\t")
+print("\t\t\t\t B     B\t\t\t\t\t\t")
+print("\t\t\t\t   EEE\t\t\t\t\t\t\t")
