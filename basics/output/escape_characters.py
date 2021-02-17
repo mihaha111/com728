de7e1f74304ef4d25fd8903ba3b24f41af48c886
Mihaha111 <5handm53@solent.ac.uk>
2021-02-16 20:33:06 -0800

    display multiple message

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,9 @@
+# Added Code to Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+print()
+print("\n")
+print("\n\t\t\t\"I am Beep!\"")
