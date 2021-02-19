d19e22875d37c36b788684305b20dd37c688b087
Mihaha111 <5handm53@solent.ac.uk>
2021-02-18 18:38:51 -0800

    # Added Code to Review Beep

---
+++ basics/input/review_input-output.py
@@ -0,0 +1,19 @@
+# Beep feel better
+print("Thank you for saving me! I am Beep and I am a robot. What is your name?")
+name = input()
+print(f"Nice to meet you {name}.")
+print("As a reward I would like to show you one of my tricks.")
+print("For any number you choose from 1 to 10, if you follow 4 math operations, the result will be always 3")
+print("Think to a number:")
+number = int(input())
+print("duplicate it")
+print(f"you shoud have {number*2}")
+print("\nNow, add 6 to it and then divide by 2.")
+print(f"is it {((number*2)+6)/2}?")
+print("\nAt last, now take from it the number you have initially though")
+print(f"\n\t Hurrey! It is {((number*2)+6)/2-number}!!! 👨")
+print("Did you like my trick? How many stars will you give me?")
+stars = int(input())
+print(f"Well, these are for you: {stars*'🌻'}. Have a lovely day! Beep 😜")
+
+
