6ebd9b5490d6feab084b8523ffd8279763104b00
Mihaha111 <5handm53@solent.ac.uk>
2021-02-21 14:47:28 -0800

    # Added Code to demonstrate the use of a nested decision statement

---
+++ basics/decisions/nested_decision/nestception.py
@@ -0,0 +1,29 @@
+#Looking for batteries
+print("Where should I look for batteries: bedroom, bathroom or lab?")
+place = input()
+if place == 'bedroom':
+    print("Where in the bedroom should I look: under the bed?")
+    under_the_bed = input()
+    if under_the_bed == 'yes':
+        print("Found some shoes, but no batteries.")
+    else:
+        print("In this bedroom found only mess, no batteries!")
+elif place == 'bathroom':
+    print("Where in the bathroom should I look: in the bathtub?")
+    bathtube = input()
+    if bathtube == 'yes':
+        print("Found a rubber duck, but no batteries!")
+    else:
+        print("Is the door locked?")
+elif place == 'lab':
+    print("Where should I look: under the table?")
+    table = input()
+    if table == 'yes':
+        print("Yes! I found my battery!")
+    else:
+        print("Found some tools, but no batteries.")
+else:
+    print(f"\nI don't think the batteries could be in the {place}, but I can try.")
+
+
+
---
+++ basics/decisions/nested_decision/question.py
@@ -0,0 +1,8 @@
+#topings for a something
+c1 = 'chocolate'
+c2 = 'jem'
+c3 = 'cream'
+val1 = input()
+val2 = input()
+val3 = input()
+# how to choose if only one is yes and the other two are no?
